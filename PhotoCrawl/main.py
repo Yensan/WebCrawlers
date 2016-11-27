27c63ec65ef2ef96c541de29cd14c4650748602c
Yensan <yensan@live.cn>
2016-11-27 17:28:36 +0800

    new

--- PhotoCrawl/main.py
+++ PhotoCrawl/main.py
@@ -8,7 +8,16 @@
 # BOT_NAME:就是项目目录。spiders_modules：spider模块所在的目录。
 # ITEM_PIPELINES:  指明一个Pipe类的路径
 
-#####  更改目标网站，配置说明: ##########
+
+###########  Pycharm，运行配置说明: ##########
+###### 用main.py驱动运行
+#from scrapy import cmdline
+#cmdline.execute("scrapy crawl PhotoSpider".split())
+## PhotoSpider是class的属性，name="PhotoSpider"，然后直接运行main.py
+###### 在Pycharm直接运行rosi（spider）
+## //TODO
+
+###########  更改目标网站，配置说明: ##########
 ###### 存储路径配置
 ## settings.py，更改IMAGES_STORE = '../../data/rosi/'，改成'../../data/XXX/'
 ###### 网址配置
